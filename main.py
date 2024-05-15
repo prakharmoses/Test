1e2d26c6f0dbfe6d016956ba5c1baabda6ebcccb
prakharmoses <113628188+prakharmoses@users.noreply.github.com>
2024-05-15 11:41:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Contains main code
-import pandas as pd
+import os
 import numpy as np
 
 class Dog:
@@ -22,6 +22,5 @@ def total_age(df):
 
 a = np.arays([[1,2,3,5,6,9,8]])
 b = {"Name": ["Vishu", "Kunal", "Tashu", "Am", "KRTK"], "Age": [20, 21, 22, 19, 19.5], "Grade": ["S", "S", "S", "A", "A"]}
-df = pd.DataFrame(b)
-print(df)
-print(total_age(df))
+print(b)
+print(a)
